5650e42381382e761be13e45c0fb78147d33694c
li1553770945 <1553770945@qq.com>
2024-04-12 22:53:32 +0800

    因多设备同步需要，临时提交

--- biz/internal/domain/project.go
+++ biz/internal/domain/project.go
@@ -2,13 +2,23 @@ package domain
 
 import (
 	"personal-page-be/biz/internal/do"
+	"time"
+)
+
+type ProjectStatus int
+
+const (
+	Completed ProjectStatus = iota + 1
+	InProgress
+	Discontinued
 )
 
 type ProjectEntity struct {
 	do.BaseModel
-	Name   string `vd:"len($)>5" json:"name"`
-	Desc   string `vd:"len($)>5" json:"desc"`
-	Status string `vd:"len($)>2" json:"status"`
-	Link   string `vd:"len($)>5" json:"link"`
-	Period string `vd:"len($)>5" json:"period"`
+	Name      string        `vd:"len($)>5" json:"name"`
+	Desc      string        `vd:"len($)>5" json:"desc"`
+	Link      string        `vd:"len($)>5" json:"link"`
+	Status    ProjectStatus `json:"status"`
+	StartDate time.Time     `json:"start_date"`
+	EndDate   *time.Time    `json:"end_date,omitempty"`
 }
